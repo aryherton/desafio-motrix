0853e4f79fdd9948ee69e645304b84c2b92032e6
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-27 16:26:03 -0300

    feat: created file "users.ts" in folder "./src/database/schemas"

---
+++ backend/src/database/schemas/users.ts
@@ -0,0 +1,19 @@
+import { string } from 'joi';
+import { Schema } from 'mongoose';
+
+import IUser from '../interface/IUser';
+
+export default new Schema<IUser>(
+  {
+    name: { type: String, required: true},
+    email: { type: String, required: true},
+    password: { type: String, required: true},
+    arrMessage: [
+      {
+        id: Number,
+        description: String,
+        status: String,
+        priority: Number,
+        date: String,
+      }]
+  });
