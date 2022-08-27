a5e1f1446bfffb72ebaea77890851df743026682
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-27 16:29:04 -0300

    feat: created file "LoginRouter.ts" in folder "./src/routes"

---
+++ backend/src/routes/LoginRoute.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+import LoginCtrll from '../controller/LoginCtrll';
+import ValidLogin from '../middlewares/ValidLoginMidd';
+
+const ctrll = new LoginCtrll();
+const router = Router();
+
+export default router
+  .get('/', ValidLogin.validLoginBody, ctrll.checkLogin)
+  .post('/', ValidLogin.validLoginRegister, ctrll.createLogin);
