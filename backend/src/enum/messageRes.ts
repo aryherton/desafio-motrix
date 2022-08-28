901ddc7a0b6e67a2b70629b78146a92f4571693b
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-28 17:18:40 -0300

    refactor: change message "ERROR_SERVER" in file "./src/enum/messageRes.ts"

--- backend/src/enum/messageRes.ts
+++ backend/src/enum/messageRes.ts
@@ -4,10 +4,11 @@ enum Messages {
   UPDATE_SUCCESS = 'Match updated successfully',
   SUCCESS_RECORD = 'User registered successfully',
   LOGIN_SUCCESS = 'Login realized success',
-  ERROR_SERVER = 'Sorry error found',
+  ERROR_SERVER = 'Internal server error',
   BODY_IS_REQUIRED = 'Body is required in the request',
   ALREADY_REGISTERED = 'User already registered',
-  NOT_TOKEN = 'Token is requested'
+  NOT_TOKEN = 'Token is requested',
+  DELETE_SUCCESS = 'Message successfully deleted',
 }
 
 export default Messages;
