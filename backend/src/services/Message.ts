b5c8a79e7111d676b9d3a8f0b034c28789c66390
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-28 17:12:31 -0300

    feat: inserted method "updateMessage" in file "./src/services/Message.ts"

--- backend/src/services/Message.ts
+++ backend/src/services/Message.ts
@@ -1,16 +1,30 @@
 import UsersModel from '../database/model/UsersModel';
 import IMessage from '../database/interface/IMessage';
+import IUser from '../database/interface/IUser';
 
 export default class Message {
   constructor(private userModel = new UsersModel()){}
 
- async setMessage(email: string, arrMessage: IMessage[]) {
+ async setMessage(email: string, arrMessage: IMessage[]): Promise<void> {
   await this.userModel.updateMessage(email, arrMessage);
- }
+  };
 
- async getMessage(email: string) {
+ async getMessage(email: string): Promise<IUser> {
   const [ user ] = await this.userModel.getByEmail(email);
   
-  return user.arrMessage;
- }
+  return user;
+  };
+  
+  async updateMessage(email: string, arrMessage: IMessage[]) {
+    await this.userModel.updateMessage(email, arrMessage);
+  };
+
+  async deleteMessage(email: string, arrMessDel: string[]): Promise<void> {
+    const user = await this.getMessage(email);    
+    const newArrMessage = user
+      .arrMessage?.filter((mess) => arrMessDel
+        .every((messDel) => mess._id?.toString() !== messDel));
+    
+    await this.userModel.updateMessage(email, newArrMessage || []);
+  };
 }
