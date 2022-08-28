fa22bcd22163a2d3d4c8a3781d2ca038062dca90
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-28 17:26:07 -0300

    refactor: inserte type "NextFuntion" to method checkLoign in file "./src/controller/LoginCtrll.ts"

--- backend/src/controller/LoginCtrll.ts
+++ backend/src/controller/LoginCtrll.ts
@@ -1,8 +1,9 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 import UserService from '../services/User';
 import StatusHttp from '../enum/statusHttp';
 import MessagesRes from '../enum/messageRes';
+import Erros from '../error/ClassErrors';
 
 export default class LoginCtrll {
   constructor(private userServ = new UserService()) {}
@@ -24,7 +25,11 @@ export default class LoginCtrll {
     }
   }
 
- checkLogin =  async (req: Request, res: Response): Promise<Response> => {
+  checkLogin = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<Response | void> => {
     try {
         const { email, password } = req.body;
         const token = await this.userServ.validUser(email, password);
@@ -32,11 +37,11 @@ export default class LoginCtrll {
         if (token) {
           return res.status(StatusHttp.OK).json({ message: MessagesRes.LOGIN_SUCCESS, token });
         }
-
-      return res.status(StatusHttp.UNAUTHORIZED).json({ message: MessagesRes.INCORRECT });
-    } catch(e) {
+      throw Erros.notFound(MessagesRes.INCORRECT);
+    } catch(e: any) {
       console.log(e);
-      return res.status(StatusHttp.SERVER_ERROR).json({ message: MessagesRes.ERROR_SERVER});
+      // next(e);
+      return res.status(StatusHttp.NOT_FOUND).json({ message: e.message });
     }
   }
 }
