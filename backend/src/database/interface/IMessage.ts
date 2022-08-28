b603636eb8a3a453d5a9e84a6fae471f45031105
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-28 18:55:39 -0300

    feat: created interface "HistoryUpdateMessage" in file "./src/database/interface/IMessage.ts"

--- backend/src/database/interface/IMessage.ts
+++ backend/src/database/interface/IMessage.ts
@@ -1,7 +1,15 @@
+interface HistoryUpdateMessage {
+  title: string;
+  description: string,
+  status: string,
+  priority: string,
+};
+
 export default interface MessageToDoList {
     _id?: string;
     title: string;
     description: string,
     status: string,
     priority: string,
-  }
+    historyUpdate: HistoryUpdateMessage[],
+};
