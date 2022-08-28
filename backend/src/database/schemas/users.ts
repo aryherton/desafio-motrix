b04da1c8d4020e166e2fd2e1dc47c687095c6a77
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-28 17:19:27 -0300

    refactor: change in "Schema<IUser>" in file "./srsc/database/schemas"

--- backend/src/database/schemas/users.ts
+++ backend/src/database/schemas/users.ts
@@ -5,15 +5,32 @@ import IUser from '../interface/IUser';
 
 export default new Schema<IUser>(
   {
-    name: { type: String, required: true},
-    email: { type: String, required: true},
-    password: { type: String, required: true},
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    manager: { type: Boolean, default: false },
     arrMessage: [
       {
-        id: Number,
+        title: String,
         description: String,
         status: String,
         priority: Number,
-        date: String,
-      }]
+        timestamp: {
+          created: { type: Date, default: Date.now },
+          // updated: { type: Date, default: Date.now },
+        },
+      }
+    ],
+    historyUpdateMessage: [
+      {
+        title: String,
+        description: String,
+        status: String,
+        priority: Number,
+        timestamp: {
+          // created: { type: Date, default: Date.now },
+          updated: { type: Date, default: Date.now },
+        },
+      },
+    ]
   });
