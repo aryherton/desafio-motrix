768e959e474bd7b947a5afc602f2c4ea4b2e0259
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-29 23:40:28 -0300

    refector: removed method "updateMessage" in file "./src/database/model/UserModel.ts"

--- backend/src/database/model/UsersModel.ts
+++ backend/src/database/model/UsersModel.ts
@@ -14,11 +14,11 @@ export default class UsersModel {
   };
 
   async getAll(): Promise<IUser[]> {
-    return this.model.find({}, { password: 0, updatedAt: 0, __v: 0 });
+    return this.model.find({}, { password: 0 });
   };
 
-  async getByEmail(email: string): Promise<IUser[]> {
-    return this.model.find({ email }, { password: 0, updatedAt: 0, __v: 0 });
+  async getByEmail(email: string): Promise<IUser> {
+    return this.model.findOne({ email }, { password: 0 }) as unknown as Promise<IUser>;
   };
 
   async getPassword(email: string): Promise<string | void> {
@@ -29,12 +29,8 @@ export default class UsersModel {
     }
   };
 
-  async updateMessage(email: string, arrMessage: IMessage[]): Promise<void> {
-    await this.model.updateOne({ email }, { arrMessage });
-  };
-
   async updateUser(email: string, user: IUser): Promise<void> {
-    await this.model.updateOne({ email }, { ...user });
+    await this.model.updateOne({ email }, user);
   };
 
   async deleteUser(id: string): Promise<void> {
