df726a99d0aa2a074ca26174d593a807b3c37e74
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-29 23:42:49 -0300

    refector: renamed messages "SUCCESS_RECORD" in file "./src/enum/messageRes.ts"

--- backend/src/enum/messageRes.ts
+++ backend/src/enum/messageRes.ts
@@ -2,7 +2,7 @@ enum Messages {
   INCORRECT = 'Incorrect email or password',
   INVALID = 'All fields must be filled',
   UPDATE_SUCCESS = 'Match updated successfully',
-  SUCCESS_RECORD = 'User registered successfully',
+  SUCCESS_RECORD = 'Message registered successfully',
   LOGIN_SUCCESS = 'Login realized success',
   ERROR_SERVER = 'Internal server error',
   BODY_IS_REQUIRED = 'Body is required in the request',
