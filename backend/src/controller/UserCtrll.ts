dd22f7fd713aaf1ab2ea15eaaa4fa98273624b52
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-29 23:36:52 -0300

    feat: created file "UserCtrll.ts" in file "./src/contorller"

---
+++ backend/src/controller/UserCtrll.ts
@@ -0,0 +1,77 @@
+import { Request, Response, NextFunction } from 'express';
+
+import UserService from '../services/User';
+import MessageService from '../services/Message';
+import StatusHttp from '../enum/statusHttp';
+import MessagesRes from '../enum/messageRes';
+import Erros from '../error/ClassErrors';
+import { JwtPayload } from 'jsonwebtoken';
+import Token from '../help/Token';
+import IUser from '../database/interface/IUser';
+
+export default class UserCtrll {
+  constructor(
+    private userServ = new UserService(),
+    private messageServ = new MessageService(),
+  ) {}
+
+  createUser = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const { name, email, password } = req.body;
+      const checkUser = await this.userServ.getUserByEmail(email);
+
+      if (checkUser) {
+        return res.status(StatusHttp.UNAUTHORIZED).json({ message: MessagesRes.ALREADY_REGISTERED });
+      }
+      const token = await this.userServ.insertUser({ name, email, password });
+
+      return res.status(StatusHttp.CREATED).json({ message: MessagesRes.LOGIN_SUCCESS, token });
+    } catch (e) {
+      console.log(e);
+      return res.status(StatusHttp.SERVER_ERROR).json({ message: MessagesRes.ERROR_SERVER });
+    }
+  };
+
+  checkLogin = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<Response | void> => {
+    try {
+      const { email, password } = req.body;
+      const token = await this.userServ.validUser(email, password);
+
+      if (token) {
+        return res.status(StatusHttp.OK).json({ message: MessagesRes.LOGIN_SUCCESS, token });
+      }
+      throw Erros.notFound(MessagesRes.INCORRECT);
+    } catch (e: any) {
+      console.log(e);
+      // next(e);
+      return res.status(StatusHttp.NOT_FOUND).json({ message: e.message });
+    }
+  };
+  
+  getUser = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const { authorization } = req.headers;
+      
+      if (authorization) {
+        const user = Token.validToken(authorization) as JwtPayload;
+        const userData: IUser = await this.userServ.getUserByEmail(user.email);
+        const arrMessage = await this.messageServ.getAllMessage(user._id);
+
+        return res.status(StatusHttp.OK).json({
+          name: userData.name,
+          email: userData.email,
+          arrMessage
+        });
+      }
+
+      return res.status(StatusHttp.NO_CONTENT).json({ message: MessagesRes.NOT_TOKEN });
+    } catch (e) {
+      console.log(e);
+      return res.status(StatusHttp.SERVER_ERROR).json({ message: MessagesRes.ERROR_SERVER });
+    }
+  };
+}
