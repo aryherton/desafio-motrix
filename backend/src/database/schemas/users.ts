39bd444a3a5f7e747eff20fe5362f6088c3161c0
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-29 23:41:53 -0300

    refector: change interface "IUser" in file "./src/database/schemas"

--- backend/src/database/schemas/users.ts
+++ backend/src/database/schemas/users.ts
@@ -8,29 +8,4 @@ export default new Schema<IUser>(
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    manager: { type: Boolean, default: false },
-    arrMessage: [
-      {
-        title: String,
-        description: String,
-        status: String,
-        priority: Number,
-        timestamp: {
-          created: { type: Date, default: Date.now },
-          // updated: { type: Date, default: Date.now },
-        },
-        historyUpdate: [
-          {
-            title: String,
-            description: String,
-            status: String,
-            priority: Number,
-            timestamp: {
-              // created: { type: Date, default: Date.now },
-              updated: { type: Date, default: Date.now },
-            },
-          },
-        ]
-      }
-    ],
   });
