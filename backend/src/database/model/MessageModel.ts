c9588d15e4994ded70f918d8da1635201cab9532
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-29 23:39:05 -0300

    feat: created file "./src/database/model"

---
+++ backend/src/database/model/MessageModel.ts
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+import MessageSchemas from '../schemas/message';
+import IMessage from '../interface/IMessage';
+
+export default class MessageModel {
+  constructor(
+    private model = mongoose.model('messages', MessageSchemas)
+  ) { };
+  
+  async create(message: IMessage): Promise<IMessage & { _id: mongoose.Types.ObjectId }> {
+    return this.model.create(message);
+  };
+
+  async getAll(idUser: string): Promise<IMessage[]> {
+    const message = await this.model.find({ idUser });
+
+    return message;
+  };
+
+  async getById(id: string): Promise<IMessage | void> {
+    const _id = new mongoose.Types.ObjectId(id);
+    const message = await this.model.findOne({ _id });
+
+    if (message) {
+      return message;
+    }
+  };
+
+  async updateMessage(id: string, message: IMessage): Promise<void> {
+    const _id = new mongoose.Types.ObjectId(id);
+    await this.model.updateOne({ _id }, message);
+  };
+
+  async deleteMessage(id: string): Promise<void> {
+    const _id = new mongoose.Types.ObjectId(id);
+    await this.model.deleteOne({ _id });
+  };
+}
