df05ccda67283cea7ac35433578ac3e30773def3
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-31 22:27:44 -0300

    feat: created file "api.ts" in folder "./src/utils"

---
+++ front/src/utils/api.ts
@@ -0,0 +1,49 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+
+import { ITasks } from '../interface/ITasks';
+import { IUser } from '../interface/IUser';
+
+const api = axios.create({
+  baseURL: `https://desafio-motrix.herokuapp.com/`,
+});
+
+type GetToken = {
+  token: string;
+}
+
+export const loginGetToken = async (endPoint: string, body: IUser): Promise<string> => {
+  try {
+    const token = await api.post(endPoint, body)
+      .then((response) => response.data.token);
+
+    return token;
+  } catch (error) {
+    return error.response.status;
+  }
+}
+
+export const registerUser = async (endPoint: string, body: IUser): Promise<string> => {
+  try {
+    const data = await api.post(endPoint, body)
+      .then((resp) => resp.data);
+
+    return data.token;
+  } catch (error) {
+    return error.response.status;
+  }
+};
+
+export const setTasks = async (endPoint: string, body: ITasks, token: string) => {
+  api.defaults.headers.common['Authorization'] = token;
+  await api.post(endPoint, body);
+}
+
+export const getDatas = async (endPoint: string, token: string): Promise<IUser> => {
+  api.defaults.headers.common['Authorization'] = token;
+  const cartProduct = await api.get(endPoint)
+    .then((resp) => resp.data);
+
+  return cartProduct;
+}
+
+export default api;
