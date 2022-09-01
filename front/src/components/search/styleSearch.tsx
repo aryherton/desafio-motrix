e7abdae030c3118f93cbe6c20b43011a270ba417
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-09-01 18:13:39 -0300

    feat: created file "styleSearch.tsx" in folder "./src/components/search"

---
+++ front/src/components/search/styleSearch.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const SearchWrapper = styled.div`
+  display: flex;
+
+  width: 60%;
+  height: 30px;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`
