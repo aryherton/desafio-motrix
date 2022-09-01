bd6efc56f8b0f90c33d23275776dc159dc36666c
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-31 22:23:05 -0300

    feat: import "FormLogin" in file "./src/pages/index.tsx"

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -3,23 +3,17 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 import FormLogin from '../components/formLogin/FormLogin'
-import CorrectImg from '../assets/images/correct.png'
+import { LoginWrapper } from './styles/pagesStyle/styledLogin'
+import Logo from '../assets/images/logo.png'
 
 function Login(): JSX.Element {
   return (
-    <div>
-      <Head>
-        <title>Homepage</title>
-      </Head>
-
+    <LoginWrapper>
       <main>
-        <h1>
-          Welcome to
-        </h1>
-        <Image src={CorrectImg} />
+        <Image src={Logo} alt="img_logo" />
         <FormLogin />
       </main>
-    </div>
+    </LoginWrapper>
   )
 }
 
