59db95f38531285e5d03ce56762a7f0573ce01c1
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-09-01 18:18:05 -0300

    refactor: import "filterSearchReducer" in file "./src/redux/store"

--- front/src/redux/store/store.ts
+++ front/src/redux/store/store.ts
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import userReducer from '../slice/userSlice';
+import filterSearchReducer from '../slice/filterSearch';
+import tasksSlice from '../slice/tasksSlice';
 
 export default configureStore({
   reducer: {
     user: userReducer,
+    filterSearch: filterSearchReducer,
+    tasks: tasksSlice,
   },
 })
