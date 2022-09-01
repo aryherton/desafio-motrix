eba040912219dc478232eb341c19c8211e3f2efd
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-09-01 18:16:22 -0300

    refactor: import "useDispatch" in file "./src/pages/home.tsx"

--- front/src/pages/home.tsx
+++ front/src/pages/home.tsx
@@ -1,20 +1,31 @@
 import router from 'next/router'
 import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 
 import Header from '../components/header/Header'
+import Menu from '../components/menu/Menu'
+import { getDatas } from '../utils/api'
+import { changeAllTasks } from '../redux/slice/tasksSlice'
 
 function Home(): JSX.Element {
+  const dispatch = useDispatch()
   useEffect(() => {
     const userStorage = JSON.parse(localStorage.getItem('user'))
 
     if (userStorage === null || !userStorage.token) {
       router.push('/')
     }
+
+    (async () => {
+      const user = await getDatas('user/message', userStorage.token)
+      dispatch(changeAllTasks(user.arrMessage))
+    })()
   }, [])
 
   return (
     <div>
       <Header />
+      <Menu />
     </div>
   )
 }
