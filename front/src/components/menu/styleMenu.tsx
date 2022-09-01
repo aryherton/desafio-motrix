16f7783f7198b207548d6b1434888dda59d9ffdd
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-09-01 18:12:40 -0300

    feat: created file "styleMenu.tsx" in folder "./src/components/menu"

---
+++ front/src/components/menu/styleMenu.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const MenuWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 110px;
+  background-color: yellow;
+`
