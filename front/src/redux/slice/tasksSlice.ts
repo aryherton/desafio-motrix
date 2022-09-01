f26e27696c6b93f05958c75ea980d2b3307f59cc
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-09-01 18:17:28 -0300

    feat: created file "tasksSlice.ts" in folder "./src/redux/slice"

---
+++ front/src/redux/slice/tasksSlice.ts
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const slice = createSlice({
+  name: 'filterSearch',
+  initialState: {
+    allTasks: [],
+    filterTasks: [],
+  },
+
+  reducers: {
+    changeAllTasks: (state, { payload }) => {
+      return { ...state, allTasks: payload };
+    },
+    changeFilterTasks: (state, { payload }) => {
+      return { ...state, filterTasks: payload };
+    },
+  },
+})
+
+export const {
+  changeAllTasks,
+  changeFilterTasks,
+} = slice.actions;
+
+export const selectAllTasks = (state: { allTasks: any; }) => state.allTasks;
+export const selectFilterTasks = (state: { filterTasks: any; }) => state.filterTasks;
+
+export default slice.reducer;
