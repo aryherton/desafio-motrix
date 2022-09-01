2f3c7c308e6c4983122b41b4c1d3b0686d240c2e
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-09-01 12:57:42 -0300

    refactor: import "useRouter" in file "./src/pages/index.tsx"

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import Head from 'next/head'
+import React, { useEffect } from 'react'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 
 import FormLogin from '../components/formLogin/FormLogin'
 import { LoginWrapper } from './styles/pagesStyle/styledLogin'
 import Logo from '../assets/images/logo.png'
 
 function Login(): JSX.Element {
+  const router = useRouter()
+
+  useEffect(() => {
+    const userStorage = JSON.parse(localStorage.getItem('user'))
+
+    if (userStorage !== null && userStorage.token) {
+      router.push('/home')
+    }
+  }, [])
+
   return (
     <LoginWrapper>
       <main>
