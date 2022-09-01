543ca47cd16af876933510a9916137f8cb324478
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-31 22:24:59 -0300

    feat: created file "styledLogin.ts" in folder "./src/pages/styles/pagesStyle/styledLogin.ts"

---
+++ front/src/pages/styles/pagesStyle/styledLogin.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const LoginWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100vw;
+  height: 100vh;
+  background: rgb(255,254,251);
+  background: linear-gradient(90deg, rgba(255,254,251,1) 0%, rgba(247,247,24,1) 34%, rgba(250,132,71,1) 100%);
+`
