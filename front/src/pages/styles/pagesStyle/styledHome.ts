61f9fcfb76b580b48cd508afa4c9c674bd1cba30
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-09-01 23:47:33 -0300

    feat: created file "styledHome.ts" in folder "./src/pages/styles/pagesStyle"

---
+++ front/src/pages/styles/pagesStyle/styledHome.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const HomeWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 100vw;
+  height: 100vh;
+
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    width: 100%;
+    height: 100%;
+  }
+`
