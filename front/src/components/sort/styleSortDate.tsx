0bf7a9062e495d33436820734c17bd54e60cec43
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-09-02 17:41:36 -0300

    feat: created file "styleSortDate.tsx" in folder "./src/components/sort/styleSortDate.tsx"

---
+++ front/src/components/sort/styleSortDate.tsx
@@ -0,0 +1,113 @@
+import styled from 'styled-components'
+
+export const SortDateWrapper = styled.div`
+@media screen {
+  @media ( min-width: 50px ) and ( max-width: 480px ) {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    width: 60%;
+    height: 60px;
+    margin-top: 5px;
+
+    .shortButtons {
+      width: 220px;
+
+      .button_sort_date {
+        background-color: #1f7fbf;
+        border: 1px solid #416df1;
+        border-radius: 5px;
+        font-size: 11px;
+        font-weight: 600;
+        color: #fff;
+
+        width: 80px;
+        height: 30px;
+        margin-left: 20px;
+      }
+    }
+  }
+
+  @media ( min-width: 480px ) and ( max-width: 768px ) {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    width: 60%;
+    height: 60px;
+    margin-top: 5px;
+
+    .shortButtons {
+      width: 220px;
+
+      .button_sort_date {
+        background-color: #1f7fbf;
+        border: 1px solid #416df1;
+        border-radius: 5px;
+        font-size: 11px;
+        font-weight: 600;
+        color: #fff;
+
+        width: 80px;
+        height: 30px;
+        margin-left: 20px;
+      }
+    }
+  }
+
+  @media ( min-width: 769px ) and ( max-width: 1279px ) {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    width: 60%;
+    height: 60px;
+    margin-top: 5px;
+
+    .shortButtons {
+      width: 220px;
+
+      .button_sort_date {
+        background-color: #1f7fbf;
+        border: 1px solid #416df1;
+        border-radius: 5px;
+        font-size: 11px;
+        font-weight: 600;
+        color: #fff;
+
+        width: 80px;
+        height: 30px;
+        margin-left: 20px;
+      }
+    }
+  }
+
+  @media (min-width: 1279px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    width: 60%;
+    height: 60px;
+    margin-top: 5px;
+
+    .shortButtons {
+      width: 220px;
+
+      .button_sort_date {
+        background-color: #1f7fbf;
+        border: 1px solid #416df1;
+        border-radius: 5px;
+        font-size: 11px;
+        font-weight: 600;
+        color: #fff;
+
+        width: 80px;
+        height: 30px;
+        margin-left: 20px;
+      }
+    }
+  }
+}
+`
