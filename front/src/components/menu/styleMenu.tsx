8f00c27ee93eda796a529cedc2f3e03540ac1643
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-09-02 17:36:06 -0300

    feat: created file "styleMenu.tsx" in folder "./src/components/menu"

--- front/src/components/menu/styleMenu.tsx
+++ front/src/components/menu/styleMenu.tsx
@@ -1,11 +1,125 @@
 import styled from 'styled-components'
 
 export const MenuWrapper = styled.section`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+@media screen {
+  @media (
+    min-width: 50px
+    ) and (max-width: 480px) {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
 
-  width: 100%;
-  height: 110px;
-`
+      width: 100%;
+      height: 140px;
+
+      .sortTodoList {
+        width: 100%;
+        height: 100px;
+
+        margin-top: 10px;
+        margin-bottom: 10px;
+
+        background-color: #c9f3c9;
+        box-shadow: 1px 1px 5px 1px #e0cd7c;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+
+        .sort {
+          display: flex;
+          justify-content: space-around;
+          width: 100%;
+        }
+      }
+    }
+
+    @media (
+    min-width: 480px
+    ) and (max-width: 768px) {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      width: 100%;
+      height: 140px;
+
+      .sortTodoList {
+        width: 100%;
+        height: 100px;
+
+        margin-top: 10px;
+        margin-bottom: 10px;
+
+        background-color: #c9f3c9;
+        box-shadow: 1px 1px 5px 1px #e0cd7c;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+
+        .sort {
+          display: flex;
+          justify-content: space-between;
+          width: 100%;
+        }
+      }
+    }
+
+    @media (
+    min-width: 769px
+    ) and (max-width: 1279px) {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      width: 100%;
+      height: 140px;
+
+      .sortTodoList {
+        width: 100%;
+        height: 100px;
+
+        margin-top: 10px;
+        margin-bottom: 10px;
+
+        background-color: #c9f3c9;
+        box-shadow: 1px 1px 5px 1px #e0cd7c;
+
+        .sort {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          width: 100%;
+        }
+      }
+    }
+
+    @media (min-width: 1279px) {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      width: 100%;
+      height: 160px;
+
+      .sortTodoList {
+        width: 100%;
+        height: 160px;
+
+        margin-top: 10px;
+        margin-bottom: 10px;
+
+        background-color: #c9f3c9;
+        box-shadow: 1px 1px 5px 1px #e0cd7c;
+
+        .sort {
+          display: flex;
+          justify-content: space-around;
+          align-items: center;
+          width: 100%;
+        }
+      }
+    }
+  }
+  `
