023fd96e86fe412a066cfb55364a3faba7c0eb0e
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-09-02 17:37:46 -0300

    feat: created "@media" in file "./src/components/search/styleSearch.tsx"

--- front/src/components/search/styleSearch.tsx
+++ front/src/components/search/styleSearch.tsx
@@ -1,12 +1,89 @@
 import styled from 'styled-components'
 
 export const SearchWrapper = styled.div`
-  display: flex;
+  @media screen {
+  @media (
+    min-width: 50px
+    ) and (max-width: 480px) {
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
 
-  width: 60%;
-  height: 30px;
+      width: 60%;
+      height: 30px;
+      margin-top: 10px;
 
-  @media (max-width: 768px) {
-    flex-direction: column;
+      .inputSearch {
+        width: 75%;
+        height: 100%;
+        border-radius: 5px;
+        border: 1px solid #000;
+        background-color: yellow;
+      }
+    }
+  }
+
+  @media screen {
+  @media (
+    min-width: 481px
+    ) and (max-width: 768px) {
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+
+      width: 50%;
+      height: 30px;
+      margin-top: 10px;
+
+      .inputSearch {
+        width: 75%;
+        height: 100%;
+        border-radius: 5px;
+        border: 1px solid #000;
+        background-color: yellow;
+      }
+    }
+  }
+
+  @media screen {
+  @media (
+    min-width: 769px
+    ) and (max-width: 1279px) {
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+
+      width: 40%;
+      height: 30px;
+      margin-top: 10px;
+
+      .inputSearch {
+        width: 75%;
+        height: 100%;
+        border-radius: 5px;
+        border: 1px solid #000;
+        background-color: yellow;
+      }
+    }
+  }
+
+  @media screen {
+  @media (min-width: 1279px) {
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+
+      width: 30%;
+      height: 30px;
+      margin-top: 10px;
+
+      .inputSearch {
+        width: 75%;
+        height: 100%;
+        border-radius: 5px;
+        border: 1px solid #000;
+        background-color: yellow;
+      }
+    }
   }
 `
