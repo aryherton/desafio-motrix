dd21c92ae3c7f8a52d879ee14a0fe2a24c81f709
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-09-02 17:39:41 -0300

    feat: import "SortPriorityWrapper" in file "./src/components/sort/SortByPriority.tsx"

--- front/src/components/sort/SortByPriority.tsx
+++ front/src/components/sort/SortByPriority.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 
 import { changeSearchStatus } from '../../redux/slice/filterSearch';
+import { SortPriorityWrapper } from './styleSortPriority';
 
 function App(): JSX.Element {
   const dispatch = useDispatch()
@@ -11,29 +12,38 @@ function App(): JSX.Element {
   }
 
   return (
-    <form>
-      <input
-        name="priority"
-        type="radio"
-        id="priority_high"
-        value="high"
-        onClick={ handleSortByPriority }
-      />
-      <input
-        name="priority"
-        type="radio"
-        id="priority_medium"
-        value="medium"
-        onClick={handleSortByPriority}
-      />
-      <input
-        name="priority"
-        type="radio"
-        id="priority_low"
-        value="low"
-        onClick={handleSortByPriority}
-      />
-    </form>
+    <SortPriorityWrapper>
+      <label htmlFor="priority_high">
+        <input
+          name="priority"
+          type="radio"
+          id="priority_high"
+          value="high"
+          onClick={ handleSortByPriority }
+        />
+        Alta
+      </label>
+      <label htmlFor="priority_medium">
+        <input
+          name="priority"
+          type="radio"
+          id="priority_medium"
+          value="medium"
+          onClick={handleSortByPriority}
+        />
+        Média
+      </label>
+      <label htmlFor="priority_low">
+        <input
+          name="priority"
+          type="radio"
+          id="priority_low"
+          value="low"
+          onClick={handleSortByPriority}
+        />
+        Baixa
+      </label>
+    </SortPriorityWrapper>
   )
 }
 
