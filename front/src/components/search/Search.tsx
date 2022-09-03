ecbebd1b0f647e20ac8bb41321cbc03e7a57a37a
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-09-03 16:03:23 -0300

    feat: import "useselector" in file "./src/components/search/Search.tsx"

--- front/src/components/search/Search.tsx
+++ front/src/components/search/Search.tsx
@@ -1,22 +1,26 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import Image from 'next/image'
 
 import { changeSearchText } from '../../redux/slice/filterSearch'
+import { changeFilterTasks } from '../../redux/slice/tasksSlice'
 import { SearchWrapper } from './styleSearch'
 import ImgSearch from '../../assets/images/search.png'
 
 function Search(): JSX.Element {
   const dispatch = useDispatch()
+  const { searchText } = useSelector((state: any) => state.filterSearch)
+  const { allTasks } = useSelector((state: any) => state.tasks)
 
   const getDataInput = (e: React.ChangeEvent) => {
     const target = e.target as HTMLInputElement
     dispatch(changeSearchText(target.value))
   }
 
-  // const handleSearch = () => {
-  //   console.log(search, 'pesquisaaaaaaaaaaaaaaaaaaaaaa')
-  // }
+  const handleSearch = () => {
+    const search = allTasks.filter((task: any) => task.title.toLowerCase().includes(searchText.toLowerCase()))
+    dispatch(changeFilterTasks(search))
+  }
 
   return (
     <SearchWrapper>
@@ -31,7 +35,7 @@ function Search(): JSX.Element {
       src={ImgSearch}
       alt="search"
       width={ 25 } height={ 25 }
-      // onClick={ handleSearch }
+      onClick={ handleSearch }
     />
     </SearchWrapper>
     )
