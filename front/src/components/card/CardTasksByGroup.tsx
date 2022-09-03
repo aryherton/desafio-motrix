05b26c266812f63b38533d58a0a19edc6dee6a45
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-09-03 16:01:06 -0300

    feat: created file "Card.taskasByGroup.tsx" in folder "./src/components/card"

---
+++ front/src/components/card/CardTasksByGroup.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CardTasksByGroup(): JSX.Element {
+  return (
+    <div>
+      <h1>CardTasksByGroup</h1>
+    </div>
+  )
+}
+
+export default CardTasksByGroup
