b533b265ec96304e1303e283367dad35ca2b250d
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-09-04 17:11:48 -0300

    feat: created file "styleTksHistoryCard.tsx" in folder "./src/components/card"

---
+++ front/src/components/card/styleTasksHistoryCard.tsx
@@ -0,0 +1,44 @@
+import styled from 'styled-components'
+
+export const TasksHistoryCardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 100%;
+  height: 100%;
+
+  #pHistory {
+    background-color: #21bc81;
+    width: 230px;
+    height: 30px;
+    padding-top: 5px;
+    border-radius: 5px 5px 0 0;
+    text-align: center;
+  }
+
+  #historyCard {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 80%;
+    height: 350px;
+    background-color: #e3ece8;
+    border: 2px solid #21bc81;
+    border-radius: 10px;
+  }
+
+  #notHistory {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 70%;
+    height: 80%;
+    font-size: 18px;
+    font-weight: 600;
+    color: #ffffff;
+    background-color: #0e5b3d;
+    margin: auto;
+  }
+`
