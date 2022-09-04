b9ac8485f749ab491273102539c85d63c0ab925e
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-09-04 17:15:29 -0300

    feat: imported "ITasks" in file "./src/redux/slice/tasksSlice.ts"

--- front/src/redux/slice/tasksSlice.ts
+++ front/src/redux/slice/tasksSlice.ts
@@ -1,10 +1,13 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+import { ITasks } from '../../interface/ITasks';
+
 export const slice = createSlice({
   name: 'tasks',
   initialState: {
     allTasks: [],
     filterTasks: [],
+    historyTasks: '',
   },
 
   reducers: {
@@ -14,15 +17,20 @@ export const slice = createSlice({
     changeFilterTasks: (state, { payload }) => {
       return { ...state, filterTasks: payload };
     },
+    changeHistory: (state, { payload }) => {
+      return { ...state, historyTasks: payload };
+    }
   },
 })
 
 export const {
   changeAllTasks,
   changeFilterTasks,
+  changeHistory,
 } = slice.actions;
 
-export const selectAllTasks = (state: { allTasks: any; }) => state.allTasks;
-export const selectFilterTasks = (state: { filterTasks: any; }) => state.filterTasks;
+export const selectAllTasks = (state: { allTasks: ITasks[]; }) => state.allTasks;
+export const selectFilterTasks = (state: { filterTasks: ITasks[]; }) => state.filterTasks;
+export const selectHistory = (state: { historyTasks: ITasks; }) => state.historyTasks;
 
 export default slice.reducer;
