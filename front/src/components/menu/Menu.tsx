656dd595a7e68cc9fd39d44a88f3252abd4c26b5
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-09-04 17:11:00 -0300

    refactor: imported img "Voltar" in file "./src/components/menu/Menu.tsx"

--- front/src/components/menu/Menu.tsx
+++ front/src/components/menu/Menu.tsx
@@ -1,22 +1,38 @@
 import React from 'react'
+import Image from 'next/image'
+import { useSelector, useDispatch } from 'react-redux'
 
 import Search from '../search/Search'
-import SortByStatus from '../sort/SortByStatus'
 import SortByDate from '../sort/SortByDate'
 import SortByPriority from '../sort/SortByPriority'
 import { MenuWrapper } from './styleMenu'
+import { changeHistory } from '../../redux/slice/tasksSlice'
+import Voltar from '../../assets/images/voltar.png'
 
 function Menu(): JSX.Element {
+  const dispatch = useDispatch()
+  const { historyTasks } = useSelector((state: any) => state.tasks)
   return (
     <MenuWrapper id="sectionMenu">
-      <Search />
+      {
+        historyTasks ? (
+          <Image
+            id="imgVoltar"
+            src={Voltar}
+            alt="voltar"
+            width={55}
+            height={55}
+            onClick={() => dispatch(changeHistory(''))}
+          />) : (
+          <Search />
+        )
+      }
       <div className="sortTodoList">
         <div className="sort">
           <SortByDate />
           <SortByPriority />
         </div>
       </div>
-      <SortByStatus />
     </MenuWrapper>
   )
 }
