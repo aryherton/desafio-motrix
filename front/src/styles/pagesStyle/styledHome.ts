e08fe9687e83f460a5c0642eeaf639bf2fc2ee87
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-09-05 14:34:57 -0300

    feat: inserted css in file "./src/styles/pagesStyle/styledHome.ts"

--- front/src/styles/pagesStyle/styledHome.ts
+++ front/src/styles/pagesStyle/styledHome.ts
@@ -11,9 +11,40 @@ export const HomeWrapper = styled.div`
   main {
     display: flex;
     flex-direction: column;
-    /* align-items: center; */
+    align-items: center;
 
     width: 100%;
     height: 100%;
+
+    #buttonDelete {
+      width: 85%;
+      height: 40px;
+      display: flex;
+      justify-content: end;
+
+      button {
+        width: 90px;
+        height: 40px;
+        background: none;
+        background-color: #eddede;
+        border-radius: 5px;
+
+        font-size: 18px;
+        font-weight: 600;
+        color: red;
+        cursor: pointer;
+      }
+
+      button:hover {
+        width: 100px;
+        font-size: 22px;
+        box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+      }
+
+      button:active {
+        width: 95px;
+        font-size: 20px;
+      }
+    }
   }
 `
