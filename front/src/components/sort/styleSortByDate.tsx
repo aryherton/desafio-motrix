976010b98956f6c3fe0cc8c8cfd147c5b8de6a7e
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-09-05 14:31:11 -0300

    feat: created file "styleSortByDate.tsx" in file "./src/components/sort/styleSortByDate.tsx"

---
+++ front/src/components/sort/styleSortByDate.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const SortDateWrapper = styled.div`
+  width: 100%;
+  height: 60px;
+  color: #000000;
+  background-color: #b3f868;
+  padding-left: 25px;
+
+  #sortDate {
+    display: flex;
+    flex-direction: column;
+
+    input {
+      margin: 0px 5px;
+    }
+  }
+`
