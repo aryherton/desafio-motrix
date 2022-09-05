8b24306f53c94cc8d3783a7c7922e293e6cb719f
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-09-04 21:49:02 -0300

    feat: imported "FormTasks" in file "./src/pages/home.tsx"

--- front/src/pages/home.tsx
+++ front/src/pages/home.tsx
@@ -1,6 +1,6 @@
 import router from 'next/router'
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import Header from '../components/header/Header'
 import Menu from '../components/menu/Menu'
@@ -8,9 +8,12 @@ import { HomeWrapper } from '../styles/pagesStyle/styledHome'
 import { getDatas } from '../utils/api'
 import { changeAllTasks } from '../redux/slice/tasksSlice'
 import TasksCard from '../components/card/TasksCard'
+import FormTasks from '../components/formTasks/FormTasks'
 
 function Home(): JSX.Element {
   const dispatch = useDispatch()
+  const { addTask } = useSelector((state: any) => state.filterSearch)
+
   useEffect(() => {
     const userStorage = JSON.parse(localStorage.getItem('user'))
 
@@ -22,14 +25,16 @@ function Home(): JSX.Element {
       const user = await getDatas('user/message', userStorage.token)
       dispatch(changeAllTasks(user.arrMessage))
     })()
-  }, [])
+  }, [addTask])
 
   return (
     <HomeWrapper>
       <Header />
       <main>
         <Menu />
-        <TasksCard />
+        {
+          addTask === true ? (<FormTasks />) : (<TasksCard />)
+        }
       </main>
     </HomeWrapper>
   )
